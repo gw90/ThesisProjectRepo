840030066597e7e4fe49d8bc1ca6f455fc0ef2a8
Gregory Wickham <gwickham99@gmail.com>
2025-09-27 15:28:06 -0700

    I have an almost perfect proof of exactly the wrong thing

--- ThesisProjectRepo/Aupetit.lean
+++ ThesisProjectRepo/Aupetit.lean
@@ -120,6 +120,8 @@ lemma aupetit_6_2_15iilemma (t : ℂ) (ht : t = conj t) (x y : A) (h : f (x * st
   rw [← two_mul, ← mul_assoc] at this
   exact this
 
+noncomputable def c : ℝ := 1 + (2:ℝ)^(1/2 : ℝ)
+
 theorem aupetit_6_2_15ii (x y : A) :
   norm (f (x * star y)) ^ 2 ≤ f (x * star x) * f (y * star y) := by
   -- have h := aupetit_6_2_15lemma f x y (f (x * star y)/((1+2^(1/2))*f (y * star y)))
@@ -129,27 +131,218 @@ theorem aupetit_6_2_15ii (x y : A) :
   have fyy := fxx_eq_conj f y
   have fxnonneg := PositiveLinearMap.map_nonneg f (mul_star_self_nonneg x)
   have fynonneg := PositiveLinearMap.map_nonneg f (mul_star_self_nonneg y)
+  have cRealSelf: (c : ℂ).re  = c := by exact rfl
+  have fyyIm0: (f (y * star y)).im  = 0 := by exact fOfxStarxHas0Im f y
+  have fxxIm0: (f (x * star x)).im  = 0 := by exact fOfxStarxHas0Im f x
+  have constEqOwnConj : conj (c : ℂ) = c := by simp
+  have constIm0 : (c : ℂ).im = 0 := by exact conj_eq_iff_im.mp constEqOwnConj
 
-  have prodreal :
-    (1 + 2^(1/2))*f (y * star y) = conj ((1 + 2^(1/2))*f (y * star y)) := by simp [fyy]
-  have prodIm0 : ((1 + 2^(1/2))*f (y * star y)).im = 0 := conj_eq_iff_im.mp (id (Eq.symm prodreal))
-  have invIm0 : (((1 + 2^(1/2))*f (y * star y))⁻¹).im = 0 := by
+  /-have prodreal :
+    (1 + (2:ℝ)^(1/2 : ℝ))*f (y * star y) = conj ((1 + (2:ℝ)^(1/2 : ℝ))*f (y * star y)) := by exact?
+  -/
+
+  have prodIm0 : (c*f (y * star y)).im = 0 := by
+    simp; right
+    exact fyyIm0
+  have invIm0 : ((c*f (y * star y))⁻¹).im = 0 := by
     simp [Complex.inv_im]
-    left
-    exact fOfxStarxHas0Im f y
+    left; left
+    exact fyyIm0
+
+  /- -- this works. I should have realize sooner, but also this should not be true >:(
+  have : 1 + 2^(1/2) = 1 + 1 := by simp-/
 
   have normIm0 : (norm (f (x * star y)) :ℂ ).im = 0 := by exact rfl
-  have fullProdIm0 := Complex.smul_im (norm (f (x * star y))) (((1 + 2^(1/2))*f (y * star y))⁻¹)
-  rw [invIm0] at fullProdIm0
-  simp at fullProdIm0
+  have fullProdIm0 := Complex.smul_im ((norm (f (x * star y)))) (c * f (y * star y))⁻¹
+
+  rw [invIm0, MulActionWithZero.smul_zero ‖f (x * star y)‖] at fullProdIm0
+  have eqOwnConj : (‖f (x * star y)‖ • (c * f (y * star y))⁻¹)
+   = conj (‖f (x * star y)‖ • (c * f (y * star y))⁻¹) :=
+     Eq.symm ((fun {z} ↦ conj_eq_iff_im.mpr) fullProdIm0)
+  have inter := aupetit_6_2_15iilemma (f)
+    (norm (f (x * star y))/(c*f (y * star y))) eqOwnConj x y
+  by_cases fzero : f (x * star y) = 0
+  · simp only [fzero, norm_zero, ofReal_zero, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true,
+    zero_pow, ge_iff_le]
+    exact Left.mul_nonneg fxnonneg fynonneg
+  push_neg at fzero
+  have h := inter fzero
+  simp at h
+  rw [add_assoc, add_comm] at h
+  have step1 := tsub_le_iff_right.mpr h
+  rw [zero_sub] at step1
+  have step2 := by calc
+    -f (x * star x) ≤
+  2 * (↑‖f (x * star y)‖ / (↑c * f (y * star y))) * ↑‖f (x * star y)‖ +
+    (↑‖f (x * star y)‖ / (↑c * f (y * star y))) ^ 2 * f (y * star y) := step1
+    _ = 2 * (↑‖f (x * star y)‖ / (↑c * f (y * star y))) * ↑‖f (x * star y)‖ +
+    (↑‖f (x * star y)‖^2 / (↑c^2 * (f (y * star y))^2)) * f (y * star y) := by ring
+    _ = 2 * (↑‖f (x * star y)‖ / (↑c * f (y * star y))) * ↑‖f (x * star y)‖ +
+    (‖f (x * star y)‖^2 / (c^2 * (f (y * star y))))*((f (y * star y))⁻¹ * f (y * star y)) := by ring
+    _ = 2 * (↑‖f (x * star y)‖ / (↑c * f (y * star y))) * ↑‖f (x * star y)‖ +
+    (‖f (x * star y)‖^2 / (c^2 * (f (y * star y)))) := by field_simp
+    _ = 2 * (↑‖f (x * star y)‖ / (↑c * f (y * star y))) * ↑‖f (x * star y)‖ +
+    (‖f (x * star y)‖^2 / ((f (y * star y))* c^2)) := by ring
+    _ = 2 * (↑‖f (x * star y)‖ / (↑c * f (y * star y))) * ↑‖f (x * star y)‖ +
+    (‖f (x * star y)‖^2 / ((f (y * star y))* c))  / c := by ring
+    _ = 2 * (↑‖f (x * star y)‖ / (↑c * f (y * star y))) * ↑‖f (x * star y)‖ +
+    (‖f (x * star y)‖^2 / ((f (y * star y))* c)) * (1/c) := by ring
+    _ = 2 * (‖f (x * star y)‖^2 / (↑c * f (y * star y))) +
+            (‖f (x * star y)‖^2 / ((f (y * star y))* c)) * (1/c) := by ring
+    _ = (2/c) * (‖f (x * star y)‖^2 / (f (y * star y))) +
+        (1/c^2) * (‖f (x * star y)‖^2 / ((f (y * star y)))) := by ring
+    _ = ((2/c) + (1/c^2)) * (‖f (x * star y)‖^2 / ((f (y * star y)))) := by ring
+    _ = ((2*c/c^2) + (1/c^2)) * (‖f (x * star y)‖^2 / ((f (y * star y)))) := by field_simp
+    _ = (((2*c+1)/c^2)) * (‖f (x * star y)‖^2 / ((f (y * star y)))) := by field_simp
+    _ = 1 * (‖f (x * star y)‖^2 / ((f (y * star y)))) := by
+        dsimp [c]
+        rw [pow_two]
+        norm_cast
+        rw [mul_add, mul_add, add_mul, one_mul, mul_one, mul_one, add_mul, one_mul]
+        have lem := pow_two ((2:ℝ)^((1:ℝ)/(2:ℝ)))
+        rw [← lem]
+        rw [← Real.sqrt_eq_rpow, Real.sq_sqrt]
+        nth_rw 2 [add_assoc, ← add_assoc]
+        rw [← two_mul, ← add_assoc]
+        rw [add_assoc]
+        nth_rw 2 [add_comm]
+        rw [add_comm]
+        congr
+        · field_simp
+        norm_num
+      _ = ‖f (x * star y)‖^2 / ((f (y * star y))) := by rw [one_mul]
+  by_cases fyyzero : f (y * star y) = 0
+  · sorry
+  push_neg at fyyzero
+  have step3 : -f (x * star x) * (f (y * star y)) ≤
+    ↑‖f (x * star y)‖ ^ 2 * (f (y * star y))⁻¹ * ((f (y * star y))) :=
+      mul_le_mul_of_nonneg_right step2 fynonneg
+  rw [mul_assoc] at step3
+  nth_rw 3 [mul_comm] at step3
+  rw [Complex.mul_inv_cancel fyyzero, mul_one] at step3
+  have step4 := neg_le_neg step3
+  sorry
+
+
+
+
+
+
+
+
+      /-
+       dsimp [c]; congr
+      have l1 :
+        Complex.ofReal (1 + (2:ℝ)^(1/2 : ℝ)) = (1 : ℂ) + ((2:ℝ)^(1/2 : ℝ) : ℂ) := by sorry
+      rw [l1]
+
+        have : ((2:NNReal)^(1/2 : ℝ)) ^ (2: ℝ) = 2 := by
+          rw [← (NNReal.rpow_mul 2 (1/2) (2:ℝ))]
+          field_simp
+          norm_num
+        have twopos : (2 : ℝ) = (2 : NNReal) := by exact rfl
+        nth_rw 17 [twopos]
+        norm_cast
+        rw [this]-/
+
+
+
+
+      /-
+      change
+      rw [c] at cRealSelf; dsimp [c];-- dsimp [Complex.ofReal_def]
+      apply Complex.ext_iff.mpr
+      constructor
+      . rw [cRealSelf]; exact?
+        -/
+
+
+
+
+  /-have step2 := by calc
+    0 - f (x * star x) ≤
+    2 * (↑‖f (x * star y)‖ / (2 * f (y * star y))) * ↑‖f (x * star y)‖ +
+      (↑‖f (x * star y)‖ / (2 * f (y * star y))) ^ 2 * f (y * star y) := step1
+    _ =
+    (2 * ↑‖f (x * star y)‖ / (2 * f (y * star y))) * ↑‖f (x * star y)‖ +
+      (↑‖f (x * star y)‖ / (2 * f (y * star y))) ^ 2 * f (y * star y) := by ring
+    _ =
+    (↑‖f (x * star y)‖ / (f (y * star y))) * ↑‖f (x * star y)‖ +
+      (↑‖f (x * star y)‖ / (2 * f (y * star y))) ^ 2 * f (y * star y) := by ring
+    _ =
+    (↑‖f (x * star y)‖^2 / (f (y * star y))) +
+      (↑‖f (x * star y)‖ / (2 * f (y * star y))) ^ 2 * f (y * star y) := by ring
+    _ =
+    (↑‖f (x * star y)‖^2 / (f (y * star y))) +
+      (↑‖f (x * star y)‖^2 / (2 * f (y * star y)) ^ 2) * f (y * star y) := by ring
+    _ =
+    (↑‖f (x * star y)‖^2 / (f (y * star y))) +
+      (↑‖f (x * star y)‖^2 / ((2^2) * (f (y * star y))^2)) * f (y * star y) := by ring
+    _ =
+    (↑‖f (x * star y)‖^2 / (f (y * star y))) +
+      (↑‖f (x * star y)‖^2 / ((2^2) * (f (y * star y)))) := by field_simp
+    _ =
+    ((↑‖f (x * star y)‖^2) +
+      (↑‖f (x * star y)‖^2 / ((2^2)))) / (f (y * star y)) := by field_simp
+    _ =
+    ((↑‖f (x * star y)‖^2) + (↑‖f (x * star y)‖^2 / (4))) / (f (y * star y)) := by norm_num
+    _ =
+    ((4 * ↑‖f (x * star y)‖^2)/4 + (↑‖f (x * star y)‖^2 / (4))) / (f (y * star y)) := by norm_num
+    _ =
+    ((5 * ↑‖f (x * star y)‖^2 / (4))) / (f (y * star y)) := by ring
+    _ =
+    (((5/4) * ↑‖f (x * star y)‖^2)) / (f (y * star y)) := by ring
+    _ =
+    (((5/4) * ↑‖f (x * star y)‖^2)) * (f (y * star y))⁻¹ := by ring
+
+  rw [zero_sub] at step2
+  -- auptetit is missing the case because his proof is bad
+  by_cases fyyzero : f (y * star y) = 0
+  · sorry
+  push_neg at fyyzero
+  have step3 : -f (x * star x) * (f (y * star y)) ≤
+    5 / 4 * ↑‖f (x * star y)‖ ^ 2 * (f (y * star y))⁻¹ * ((f (y * star y))) :=
+      mul_le_mul_of_nonneg_right step2 fynonneg
+  have step4 := by calc
+   -f (x * star x) * (f (y * star y)) ≤
+    5 / 4 * ↑‖f (x * star y)‖ ^ 2 * (f (y * star y))⁻¹ * ((f (y * star y))) := by exact
+      step3
+    _ = 5 / 4 * ↑‖f (x * star y)‖ ^ 2 * ((f (y * star y))⁻¹ * ((f (y * star y)))) := by ring
+    _ = 5 / 4 * ↑‖f (x * star y)‖ ^ 2 * 1 := by
+      congr
+      rw [mul_comm]
+      exact Complex.mul_inv_cancel fyyzero
+    _ = 5 / 4 * ↑‖f (x * star y)‖ ^ 2 := by rw [mul_one]
+  have step5 := neg_le_neg step4
+  simp at step5
+
+  have : 0 ≤ ‖f (x * star y)‖ := by exact norm_nonneg (f (x * star y))
+  have hyp1 : 0 ≤ ‖f (x * star y)‖ ^ 2 := by exact sq_nonneg ‖f (x * star y)‖
+  -/
+
+
+
+  /-
+  have hyp3 : (0: ℂ) ≤ ‖f (x * star y)‖ ^ 2 := by exact hyp1
+  have hyp2 : (0 : ℂ) ≤ (5/4 : ℂ) := by sorry
+  have hyp4 : (0 : ℂ)  ≤ ((5/4) * ‖f (x * star y)‖^2) := by exact mul_nonneg hyp2 hyp3
+  #check mul_le_of_le_div₀ hyp4 fynonneg
+  have : -f (x * star x) * f (y * star y) ≤ (↑‖f (x * star y)‖ ^ 2 + ↑‖f (x * star y)‖ ^ 2 / 4) := by exact?
+
+  -/
+
+
+  /-
   rcases fullProdIm0 with h1 | h2 | h3
   . have two_ne_zero : 2 ≠ 0 := by exact Ne.symm (Nat.zero_ne_add_one 1)
     rw [h1, norm_zero]
     norm_num
     exact Left.mul_nonneg fxnonneg fynonneg
-  . sorry
+  . have eqOwnConj : (norm (f (x * star y))) (((1 + 2^(1/2))*f (y * star y))⁻¹) =
+      conj (norm (f (x * star y))) (((1 + 2^(1/2))*f (y * star y))⁻¹) := by exact?
+    have inter := aupetit_6_2_15iilemma (f) (norm (f (x * star y))/((1 + 2^(1/2))*f (y * star y))) eqOwnConj x y
   sorry
-
+  -/
   /-
   have eqOwnConj :
   have
---
+++ ThesisProjectRepo/ZuplipQuestion3.lean
@@ -0,0 +1,5 @@
+import Mathlib.Analysis.CStarAlgebra.PositiveLinearMap
+import Mathlib.Data.Complex.Basic
+
+
+example : ↑(2 ^ 2⁻¹) = 2 ^ 2⁻¹ := by norm_cast
